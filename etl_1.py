37b30a1c5c655439c170f1e14d5ce579071649d4
Utkarsh Shukla <utkarshukla1@gmail.com>
2022-05-08 10:57:56 +0530

    Format code with black

--- etl_1.py
+++ etl_1.py
@@ -4,32 +4,39 @@ from abc import ABC
 from typing import List
 import sys
 
+
 class Reader(ABC):
     @abstractmethod
     def read(self):
         raise NotImplemented
+
 
 class Transformer(ABC):
     @abstractmethod
     def transform(self, data):
         raise NotImplemented
 
+
 class Writer(ABC):
     @abstractmethod
     def write(self, data):
         raise NotImplemented
 
+
 class ETLPipeline:
-    def __init__(self, reader: Reader , transformers: List[Transformer], writer: Writer) -> None:
+    def __init__(
+        self, reader: Reader, transformers: List[Transformer], writer: Writer
+    ) -> None:
         self.reader = reader
         self.transformers = transformers
         self.writer = writer
-    
+
     def run(self):
         data = self.reader.read()
         for transformer in self.transformers:
-            data= transformer.transform(data)
-        self.writer.write(data)    
+            data = transformer.transform(data)
+        self.writer.write(data)
+
 
 class FileReader(Reader):
     def __init__(self, filepath) -> None:
@@ -39,22 +46,24 @@ class FileReader(Reader):
         with open(self.filepath) as fd:
             return fd.read()
 
+
 class TransfromUpper(Transformer):
     def transform(self, data):
         return data.upper()
 
+
 class FileWriter(Writer):
-    def __init__(self,filepath) -> None:
+    def __init__(self, filepath) -> None:
         self.filepath = filepath
 
     def write(self, data):
-        with open(self.filepath, 'w') as fw:
+        with open(self.filepath, "w") as fw:
             fw.write(data)
 
 
 def main(args):
     if len(args) < 3:
-        print('Usage: etl.py <input filepath> <output filepath>')
+        print("Usage: etl.py <input filepath> <output filepath>")
         exit()
 
     inputfile = args[1]
@@ -62,10 +71,9 @@ def main(args):
     freader = FileReader(inputfile)
     fwriter = FileWriter(outputfile)
     pipeline = ETLPipeline(
-        reader = freader,
-        transformers = [TransfromUpper(),],
-        writer = fwriter,
-     )
+        reader=freader, transformers=[TransfromUpper()], writer=fwriter
+    )
     pipeline.run()
+
 
 main(sys.argv)
--- etl_2.py
+++ etl_2.py
@@ -1,35 +1,33 @@
 from collections import defaultdict
 import sys
-from etl_1 import FileReader,FileWriter,Transformer,ETLPipeline
+from etl_1 import FileReader, FileWriter, Transformer, ETLPipeline
+
 
 class WordCounter(Transformer):
     def transform(self, data):
-        for char in [',', '.', '-', ';', '!']:
-            data = data.replace(char,'')
+        for char in [",", ".", "-", ";", "!"]:
+            data = data.replace(char, "")
         counter = defaultdict(int)
         for word in data.split():
             counter[word.lower()] += 1
-        output = ''
+        output = ""
 
         for word, count in counter.items():
-            output += f'{word} -> {count} \n'
+            output += f"{word} -> {count} \n"
         return output
 
 
 def main(args):
     if len(args) < 3:
-        print('Usage: etl.py <input filepath> <output filepath>')
+        print("Usage: etl.py <input filepath> <output filepath>")
         exit()
 
     inputfile = args[1]
     outputfile = args[2]
     freader = FileReader(inputfile)
     fwriter = FileWriter(outputfile)
-    pipeline = ETLPipeline(
-        reader = freader,
-        transformers = [WordCounter(),],
-        writer = fwriter,
-     )
+    pipeline = ETLPipeline(reader=freader, transformers=[WordCounter()], writer=fwriter)
     pipeline.run()
+
 
 main(sys.argv)
